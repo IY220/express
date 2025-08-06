32d4ccca1b19424c3f138d8401726b53dea6824d
IY220 <2201100102mv@cyber-u.ac.jp>
2025-08-06 13:06:37 +0900

    add yesno api

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
+var yesnoRouter = require('./routes/yesno');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-//
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,6 +28,7 @@ app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
 app.use('/cat', catRouter);
+app.use('/yesno', yesnoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
